480d66249da0292d76c8c53c0815dc84589ebeef
Mysterium Coniunctionis <jrc@coniunct.io>
2025-11-20 23:53:57 -0800

    Add express backend and connect frontend to APIs

--- src/index.js
+++ src/index.js
@@ -9,21 +9,26 @@ import { ThemeProvider } from './context/ThemeContext';
 import { GamificationProvider } from './context/GamificationContext';
 import { ToastProvider } from './components/common/Toast';
 import { CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <GamificationProvider>
-          <ThemeProvider>
-            <ToastProvider>
-              <CssBaseline />
-              <App />
-            </ToastProvider>
-          </ThemeProvider>
-        </GamificationProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <GamificationProvider>
+            <ThemeProvider>
+              <ToastProvider>
+                <CssBaseline />
+                <App />
+              </ToastProvider>
+            </ThemeProvider>
+          </GamificationProvider>
+        </AuthProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
